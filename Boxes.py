15e2217338468ab14ece8fe1c1fe170e60578025
titouanjehl <titouan.jehl@gmail.com>
2017-02-13 21:15:54 -0800

    bug fixing in boxes

--- Boxes.py
+++ Boxes.py
@@ -2,71 +2,83 @@ import numpy as np
 
 class Boxes( object ):
     '''This class creates the gridResolution^dimLow boxes and will compute the pairs to compare'''
-    def __init__(self, gridResolution, data):
+    def __init__(self, gridResolution):
         if not isinstance(gridResolution,int):
             raise TypeError
         if gridResolution<1:
             raise ValueError
         self.gridResolution = gridResolution
+
+    def get_min (self,data):
+        '''
+        take the data and return the minimum for each dimension in a list
+        input: numpy array
+        output: list of length dimLow
+        '''
         if not isinstance(data, np.ndarray):
             raise TypeError
         if len(data[0])>3:
             raise ValueError
-        self.data=data
-        self.len=len(data[0])
-
-    def get_min (self):
-        '''
-        take the data and return the minimum for each dimension in a list
-        output: list of length dimLow
-        '''
-        minimum=self.data[0]
-        for vec in self.data:
-            for i in range(0,self.len):
+        n=len(data[0])
+        minimum=np.copy(data[0])
+        for vec in data:
+            for i in range(0,n):
                 if vec[i]<minimum[i]:
                     minimum[i]=vec[i]
-        print minimum
         return minimum
 
-    def get_max (self):
+    def get_max (self, data):
         '''
         take the data and return the max for each dimension in a list
+        input: numpy array
         output: list of length dimLow
         '''
-        maximum=self.data[0]
-        for vec in self.data:
-            for i in range(0,self.len):
+        if not isinstance(data, np.ndarray):
+            raise TypeError
+        if len(data[0])>3:
+            raise ValueError
+        n=len(data[0])
+        maximum=np.copy(data[0])
+        for vec in data:
+            for i in range(0,n):
                 if vec[i]>maximum[i]:
                     maximum[i]=vec[i]
-        print maximum
         return maximum
 
-    def rescale_data(self):
-        maximum=self.get_max()
-        minimum=self.get_min()
+    def rescale_data(self,data):
+        if not isinstance(data, np.ndarray):
+            raise TypeError
+        if len(data[0])>3:
+            raise ValueError
+        n=len(data[0])
+        maximum=self.get_max(data)
+        minimum=self.get_min(data)
         coef=[]
         rescaled_data=[]
-        for i in range(0,self.len):
+        for i in range(0,n):
             try:
-                print maximum[i]
-                print minimum[i]
                 coef.append(float(self.gridResolution)/(float(maximum[i])-float(minimum[i])))
             except ZeroDivisionError:
                 coef.append(0)
-        for vec in self.data:
+        for vec in data:
             rescaled_vec=[]
-            for i in range(0,self.len):
-                rescaled_vec.append(int((vec[i]-minimum[i])/coef[i]))
+            for i in range(0,n):
+                rescaled_vec.append(int((vec[i]-minimum[i])*coef[i]))
             rescaled_data.append(rescaled_vec)
-        return rescaled_data
+        return np.array(rescaled_data)
 
-    def get_pairs(self):
-        rescaled_data=self.rescale_data()
+    def get_pairs(self,data):
+        if not isinstance(data, np.ndarray):
+            raise TypeError
+        if len(data[0])>3:
+            raise ValueError
+        n=len(data[0])
+        rescaled_data=rescale_data(data)
         pairs=[]
         for idx1 in range(0,len(rescaled_data)):
             for idx2 in range(idx1+1,len(rescaled_data)):
                 boolean = TRUE
-                for i in range(0,self.len):
+                for i in range(0,n):
                     boolean=boolean and (rescaled_data[idx2][i]<=rescaled_data[idx1][i]+1) and (rescaled_data[idx2][i]>=rescaled_data[idx1][i]-1)
                 if boolean:
                     pairs.append((idx1,idx2))
--- tests/test_boxes.py
+++ tests/test_boxes.py
@@ -10,29 +10,33 @@ class TestPCA( unittest.TestCase ):
 
 
     def setUp( self ):
-        self.b = Boxes( 25,np.array([[1,2,3],[4,5,6],[7,8,9],[10,0,8],[3,5,-1]]))
-
+        self.b = Boxes( 25 )
+        self.data=np.array([[1,2,3],[4,5,6],[7,8,9],[10,0,8],[3,5,-1]])
 
     def test_pca_init( self ):
         self.assertEqual( self.b.gridResolution, 25 )
-        self.assertRaises(TypeError,Boxes,'1',self.b.data)
-        self.assertRaises(ValueError,Boxes,0,self.b.data)
-        self.assertRaises(TypeError,Boxes,25,[])
-        self.assertRaises(ValueError,Boxes,25,np.array([[1,1,1,1,1,1]]))
+        self.assertRaises(TypeError,Boxes,'1')
+        self.assertRaises(ValueError,Boxes,0)
 
     def test_get_min(self):
-        minimum=np.array(self.b.get_min())
+        self.assertRaises(TypeError,self.b.get_min,[])
+        self.assertRaises(ValueError,self.b.get_min,np.array([[1,1,1,1,1,1]]))
+        minimum=np.array(self.b.get_min(self.data))
         np.testing.assert_array_almost_equal(minimum, np.array([1,0,-1]), decimal=6)
 
     def test_get_max(self):
-        maximum=np.array(self.b.get_max())
+        self.assertRaises(TypeError,self.b.get_max,[])
+        self.assertRaises(ValueError,self.b.get_max,np.array([[1,1,1,1,1,1]]))
+        maximum=np.array(self.b.get_max(self.data))
         np.testing.assert_array_almost_equal(maximum, np.array([10,8,9]), decimal=6)
 
     def test_rescale(self):
-        self.b.rescale_data()
-        minimum=np.array(self.b.get_min())
+        self.assertRaises(TypeError,self.b.rescale_data,[])
+        self.assertRaises(ValueError,self.b.rescale_data,np.array([[1,1,1,1,1,1]]))
+        rescaled_data=self.b.rescale_data(self.data)
+        minimum=np.array(self.b.get_min(rescaled_data))
         np.testing.assert_array_almost_equal(minimum, np.array([0,0,0]), decimal=6)
-        maximum=np.array(self.b.get_max())
+        maximum=np.array(self.b.get_max(rescaled_data))
         np.testing.assert_array_almost_equal(maximum, np.array([25,25,25]), decimal=6)
 
     def test_get_pairs ( self ):

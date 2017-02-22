e9a5d619b98622cd845470d5ce30091a44a52394
titouanjehl <titouan.jehl@gmail.com>
2017-02-22 15:55:28 -0800

    boxes removed, comments are still needed

--- tests/test_dim_reducer.py
+++ tests/test_dim_reducer.py
@@ -1,12 +1,14 @@
 import unittest
-import sys, os
-# add source directory to path
-sys.path.append( os.path.abspath( '..' ))
+import sys
+import os
 from DimReducer import DimReducer
+# add source directory to path
+sys.path.append(os.path.abspath('..'))
 
-class TestDimReducer(unittest.TestCase ):
+
+class TestDimReducer(unittest.TestCase):
     def setUp(self):
-        self.dr = DimReducer( 3 )
+        self.dr = DimReducer(3)
 
-    def test_dim_reduce_init( self ):
-        self.assertEqual( self.dr.dimLow, 3 )
+    def test_dim_reduce_init(self):
+        self.assertEqual(self.dr.dimLow, 3)

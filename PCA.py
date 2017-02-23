72beb5c9b634503ea7c3df4fc8f951e6f4a4b0a6
titouanjehl <titouan.jehl@gmail.com>
2017-02-23 15:01:44 -0800

    Comments and error message added to PCA

--- PCA.py
+++ PCA.py
@@ -6,16 +6,22 @@ import sklearn.decomposition
 class PCA (DimReducer):
     def __init__(self, dimLow):
         if not isinstance(dimLow, int):
-            raise TypeError
-        if dimLow < 2:
-            raise ValueError
+            raise TypeError('dimLow should be a positive integer')
+        if dimLow < 2 or dimLow > 3:
+            raise ValueError('dimLow should be 2 or 3')
         self.dimLow = dimLow
 
     def fit_transform(self, data):
+        '''
+        Usual fit_transform of numpy.decomposition.PCA
+        input: numpy array
+        output: numpy array
+        '''
         if not isinstance(data, np.ndarray):
-            raise TypeError
+            raise TypeError('Data should be a Numpy array')
         if len(data[0]) < self.dimLow:
-            raise ValueError
+            raise ValueError('Data has less collumns than dimLow')
+
         pca = sklearn.decomposition.PCA(n_components=self.dimLow,
                                         svd_solver='full')
         reducedData = pca.fit_transform(data)

9d6bf7fbb87fbf1b11863ab390182ef979261fea
Quico Spaen <quico@spaen.nl>
2017-07-14 18:05:22 -0700

    Fixes bug for pair in same box

--- SparseComputation/SparseComputation.py
+++ SparseComputation/SparseComputation.py
@@ -118,8 +118,8 @@ class SparseComputation:
                 if grid_res_basis_id == grid_res_basis_id_incremented:
                     for i in range(len(boxes_dict[box_id])):
                         for j in range(i+1, len(boxes_dict[box_id])):
-                            pairs.append(boxes_dict[box_id][i],
-                                         boxes_dict[box_id][j])
+                            pairs.append((boxes_dict[box_id][i],
+                                         boxes_dict[box_id][j]))
                 if grid_res_basis_id < grid_res_basis_id_incremented:
                     if tuple(id_incremented) in boxes_dict:
                         for a in boxes_dict[box_id]:

89765bd54ac63d7c1cf35a6030951c8146ff0e4e
Quico Spaen <quico@spaen.nl>
2019-06-26 14:45:31 -0700

    Remove test and setup requirements and format

--- setup.py
+++ setup.py
@@ -4,34 +4,32 @@ import unittest
 
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
 
 def license():
-    with open('LICENSE.md') as f:
+    with open("LICENSE.md") as f:
         return f.read()
 
 
-setup(name='sparsecomputation',
-      version='0.1dev',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.5',
-      ],
-      description='Sparse Computation for sparsifying similarity matrices',
-      keywords='pairwise similarity, classification, sparsification,' +
-      'clustering',
-      url='https://github.com/hochbaumGroup/sparsecomputation',
-      author='Titouan Jehl, Quico Spaen, Philipp Baumann',
-      author_email='qspaen@berkeley.edu',
-      license=license(),
-      long_description=readme(),
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      install_requires=['numpy', 'scipy', 'sklearn', 'six'],
-      setup_requires=['pytest-runner', ],
-      tests_require=['pytest', 'mock'],
-      zip_safe=False
-      )
+setup(
+    name="sparsecomputation",
+    version="0.1dev",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+    ],
+    description="Sparse Computation for sparsifying similarity matrices",
+    keywords="pairwise similarity, classification, sparsification," + "clustering",
+    url="https://github.com/hochbaumGroup/sparsecomputation",
+    author="Titouan Jehl, Quico Spaen, Philipp Baumann",
+    author_email="qspaen@berkeley.edu",
+    license=license(),
+    long_description=readme(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=["numpy", "scipy", "sklearn", "six"],
+    zip_safe=False,
+)

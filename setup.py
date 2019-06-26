6a144b7ee0602bc59a1da365b8f14c6227d6e029
Quico Spaen <quico@spaen.nl>
2019-06-26 16:44:01 -0700

    Update license information.

--- setup.py
+++ setup.py
@@ -3,14 +3,9 @@ from setuptools import setup, find_packages
 with open("README.md") as f:
     long_description = f.read()
 
-
-with open("LICENSE.md") as f:
-    license = f.read()
-
-
 setup(
     name="sparsecomputation",
-    version="2019.6.1",
+    version="2019.6.2",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.5",
@@ -20,7 +15,7 @@ setup(
     url="https://github.com/hochbaumGroup/sparsecomputation",
     author="Titouan Jehl, Quico Spaen, Philipp Baumann",
     author_email="qspaen@berkeley.edu",
-    license=license,
+    license="Non-commercial license. Not an open-source license.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages("src"),
